5abd5584d84002fb2083c10ee412683ee50b04a8
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-19 00:02:51 +0530

    folderStructure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="text-2xl underline text-center">Hello</div>
+      <div className="">Hello world</div>
     </>
   );
 }
