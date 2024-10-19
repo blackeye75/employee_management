e5d803eb3ab871566178f4e713e3cdfa4957944e
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-19 23:33:10 +0530

    employe

---
+++ src/component/TaskList/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div className="h-[55%] py-5 w-full bg-red-500 mt-10">
+      <div className="h-full w-[200px]">
+        
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
---
+++ src/component/other/TaskListNumber.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const TaskListNumber = () => {
+  return (
+    <div className="flex gap-3 justify-between mt-10 h-screen">
+      <div className="rounded-sm h-40 w-[45%] py-6 px-9 bg-red-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
+      <div className="rounded-sm h-40 w-[45%] py-6 px-9 bg-blue-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
+      <div className="rounded-sm h-40 w-[45%] py-6 px-9 bg-yellow-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
+      <div className="rounded-sm h-40 w-[45%] py-6 px-9 bg-indigo-400">
+        <h2 className="text-3xl font-semibold">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
+    </div>
+  );
+};
+
+export default TaskListNumber;
