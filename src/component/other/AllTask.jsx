1d0daf1c680bfa98875c00ad14736c510a46d727
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-22 01:39:56 +0530

    #

---
+++ src/component/other/AllTask.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div className="bg-[#1c1c1c] p-5 mt-5 rounded-sm">
+      <div className="bg-red-400 mt-2 py-2 px-4 flex justify-between rounded">
+        <h2>Priyansu Raj</h2>
+        <h3>Make a UI Design</h3>
+        <h3 className="">Status</h3>
+      </div>
+      <div className="bg-green-400 mt-2 py-2 px-4 flex justify-between rounded">
+        <h2>Priyansu Raj</h2>
+        <h3>Make a UI Design</h3>
+        <h3 className="">Status</h3>
+      </div>
+      <div className="bg-yellow-400 mt-2 py-2 px-4 flex justify-between rounded">
+        <h2>Priyansu Raj</h2>
+        <h3>Make a UI Design</h3>
+        <h3 className="">Status</h3>
+      </div>
+      <div className="bg-purple-400 mt-2 py-2 px-4 flex justify-between rounded">
+        <h2>Priyansu Raj</h2>
+        <h3>Make a UI Design</h3>
+        <h3 className="">Status</h3>
+      </div>
+    </div>
+  );
+};
+
+export default AllTask;
---
+++ src/component/other/CreateTask.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const CreateTask = () => {
+  return (
+    <div>
+      <form
+        action=""
+        className=" bg-zinc-900 rounded-sm flex items-center mt-5 justify-end"
+      >
+        <div className=" w-1/2 space-y-2 py-8 px-3">
+          <div>
+            <h3 className="">Task Title</h3>
+            <input
+              className="mt-2 rounded-md w-[80%] bg-transparent outline-none border-[1px]  p-1"
+              type="text"
+              placeholder="Make a UI Design"
+            />
+          </div>
+          <div>
+            <h3>Data</h3>
+            <input
+              className="mt-2 rounded-md w-[80%] bg-transparent outline-none border-[1px]  p-1"
+              type="date"
+              name=""
+              id=""
+            />
+          </div>
+          <div>
+            <h3>Assign To</h3>
+            <input
+              className="mt-2 rounded-md w-[80%] bg-transparent outline-none border-[1px]  p-1"
+              type="text"
+              placeholder="employee name"
+            />
+          </div>
+          <div>
+            <h3>Category</h3>
+            <input
+              className="mt-2 rounded-md w-[80%] bg-transparent outline-none border-[1px]  p-1"
+              type="text"
+              placeholder="Task"
+            />
+          </div>
+        </div>
+        <div className="w-1/2 space-y-2 py-8 items-end  flex flex-col ">
+          {/* <div className="w-full   flex flex-col" > */}
+          <h3 className="text-start w-full pl-36">Description</h3>
+          <textarea
+            className="mt-2 w-[80%] rounded-md bg-transparent outline-none border-[1px]"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className="px-3 py-2 bg-emerald-500 w-[80%] text-center rounded-md">
+            Create Task{" "}
+          </button>
+          {/* </div> */}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateTask;
