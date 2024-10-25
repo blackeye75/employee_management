d3963178a946fc0ae47ae6295a40c20c854fd612
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-26 01:16:25 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import EmployeDashboard from "./component/dashboard/EmployeDashboard";
 import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 
 function App() {
-  useEffect(() => {
-    // setLocalStorage();
-    getLocalStorage();
-  }, []);
+  const handelLogin = (email, password) => {
+    //  console.log(email,password);
+    if (email === "admin@me.com" && password == "123") {
+      console.log("This is admin");
+    }
+  };
+  handelLogin("admin@me.com","123");
   return (
     <div className="text-white">
       <Login />
--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -231,6 +231,8 @@ export const setLocalStorage = () => {
   localStorage.setItem("admin", JSON.stringify(admin));
 };
 export const getLocalStorage = () => {
-  const data = localStorage.getItem("employees");
-  console.log(JSON.parse(data));
+  const employees = JSON.parse(localStorage.getItem("employees"));
+  const admin = JSON.parse(localStorage.getItem("admin"));
+  console.log(admin,employees);
+ 
 };
