e840ea00a60fe28169af0312fcdfa0c67d3ff13e
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-28 23:09:30 +0530

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./App.css";
 import Login from "./component/auth/Login";
 import AdminDashboard from "./component/dashboard/AdminDashboard";
 import EmployeDashboard from "./component/dashboard/EmployeDashboard";
 import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
+import { AuthContext } from "./context/AuthProvider";
 
 function App() {
   const [user, setuser] = useState(null);
@@ -18,9 +19,13 @@ function App() {
     }
   };
   // handelLogin("user@me.com", "123");
+
+  const data =useContext(AuthContext)
+  console.log(data);
   return (
     <div className="text-white">
       {!user ? <Login handelLogin={handelLogin} /> : ""}
+      {user =="admin" ? <AdminDashboard/> : <EmployeDashboard/> }
       {/* <EmployeDashboard/> */}
       {/* <AdminDashboard/> */}
     </div>
