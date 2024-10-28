8153dca59b8fa0a96b2a8812c922e614bb3d3fca
blackeye75 <priyanshuraj22275@gmail.com>
2024-10-29 00:22:00 +0530

    understanding flow

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,26 @@ import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 import { AuthContext } from "./context/AuthProvider";
 
 function App() {
+  // setLocalStorage()
   const [user, setuser] = useState(null);
   const handelLogin = (email, password) => {
     //  console.log(email,password);
     if (email === "admin@me.com" && password == "123") {
-      console.log("This is admin");
+      setuser("admin");
     } else if (email === "user@me.com" && password == "123") {
-      console.log("This is users");
+      setuser("user");
     } else {
       console.log("Invalid Credentials");
     }
   };
   // handelLogin("user@me.com", "123");
 
-  const data =useContext(AuthContext)
+  const data = useContext(AuthContext);
   console.log(data);
   return (
     <div className="text-white">
       {!user ? <Login handelLogin={handelLogin} /> : ""}
-      {user =="admin" ? <AdminDashboard/> : <EmployeDashboard/> }
+      {user == "admin" ? <AdminDashboard /> : <EmployeDashboard />}
       {/* <EmployeDashboard/> */}
       {/* <AdminDashboard/> */}
     </div>
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,19 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { getLocalStorage } from "../utils/localStorage";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [userData, setuserData] = useState(null);
+  // const data = getLocalStorage();
+  useEffect(() => {
+    const { employees, admin } = getLocalStorage();
+    setuserData({ employees, admin });
+    console.log(employees, admin);
+  }, []);
+
   return (
-    <AuthContext.Provider value={"Hell world "}>
+    <AuthContext.Provider value={userData}>
       {children}
     </AuthContext.Provider>
   );
