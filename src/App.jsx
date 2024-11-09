944d66f1746ac2c036b467af4a3e34ce0f8e5924
blackeye75 <priyanshuraj22275@gmail.com>
2024-11-10 01:08:54 +0530

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,31 @@ function App() {
   // console.log();
   // setLocalStorage()
   const [user, setuser] = useState(null);
+
+  useEffect(() => {
+    if (authData) {
+      const loggedInUser = localStorage.getItem("loggedInUser");
+      if (loggedInUser) {
+        setuser(loggedInUser.role);
+      }
+    }
+  });
   const handelLogin = (email, password) => {
     //  console.log(email,password);
     if (email === "admin@me.com" && password == "123") {
       setuser("admin");
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
     } else if (
       authData &&
       authData?.employees.find(
-        (e) => email === e.email && password === e.password 
+        (e) => email === e.email && password === e.password
       )
     ) {
       setuser("employees");
+      localStorage.setItem(
+        "loggedInUser",
+        JSON.stringify({ role: "employee" })
+      );
     } else {
       console.log("Invalid Credentials");
     }
